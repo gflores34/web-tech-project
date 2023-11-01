81e386b3e27c1f75f6dc8433af718157b1f1690f
Gerardo Flores <63570510+gflores34@users.noreply.github.com>
2023-10-31 21:52:45 -0500

    json

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function get_all_books(){
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         var data = this.response;
-        console.log(data[0]['Title']);
+        console.log(data);
         //document.getElementById(bookId).innerHTML = this.responseText;
     }
     xhttp.open("GET", "./get_all_books.php", true);
