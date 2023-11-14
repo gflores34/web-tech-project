dbfc45334648b8e0472d677ecf1acc2962cbd012
Gerardo Flores <63570510+gflores34@users.noreply.github.com>
2023-11-14 17:33:27 -0600

    closed bold element

--- scripts/book.js
+++ scripts/book.js
@@ -5,10 +5,10 @@ function set_book_info(isbn){
         var dataParse = JSON.parse(data);
 		
 	    document.getElementById("book-title").innerHTML = "<img id=\"book-cover\" class=\"book-cover\" src=\"" + dataParse[0].ImagePath + "\" width=\"300\"></img>" + dataParse[0].Title;
-       	document.getElementById("book-author").innerHTML = "by: <b> " + dataParse[0].Name + "<b>";
-        document.getElementById("book-isbn").innerHTML = "<b>ISBN:<b> " + dataParse[0].ISBN;
-        document.getElementById("book-publisher").innerHTML = "<b>Publisher:<b> " + dataParse[0].CompanyName;
-        document.getElementById("book-pages").innerHTML = "<b>Pages:<b> " + dataParse[0].Pages;
+       	document.getElementById("book-author").innerHTML = "by: <b> " + dataParse[0].Name + "</b>";
+        document.getElementById("book-isbn").innerHTML = "<b>ISBN:</b> " + dataParse[0].ISBN;
+        document.getElementById("book-publisher").innerHTML = "<b>Publisher:</b> " + dataParse[0].CompanyName;
+        document.getElementById("book-pages").innerHTML = "<b>Pages:</b> " + dataParse[0].Pages;
         
     }
     xhttp.open("GET", "./scripts/get_book_info.php?isbn=" + isbn, true);
