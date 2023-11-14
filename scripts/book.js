2a3e115539eba861e921649c228a9b39ba0e7d45
EC2 Default User <ec2-user@ip-172-31-37-177.ec2.internal>
2023-11-14 23:28:40 +0000

    set book function for book.html

--- scripts/book.js
+++ scripts/book.js
@@ -1,16 +1,17 @@
-function set_book_page(isbn){
+function set_book_info(isbn){
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         var data = this.response;
         var dataParse = JSON.parse(data);
+		
+	document.getElementById("book-title").innerHTML = "<img id=\"book-cover\" class=\"book-cover\" src=\"" + dataParse[0].ImagePath + "\" width=\"300\"></img>" + dataParse[0].Title;
+       	document.getElementById("book-author").innerHTML = "by: <b> " + dataParse[0].Name + "<b>";
 	
         
-
-        //document.getElementById(bookId).innerHTML = this.responseText;
     }
     xhttp.open("GET", "./scripts/get_book_info.php?isbn=" + isbn, true);
     xhttp.send();
 }
 
 
-set_book_page("9781338878929");
+set_book_info("9781338878929");
