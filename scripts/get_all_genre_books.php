424463a0f95698776e679cc780d0f7ee603f6b6f
Gerardo Flores <63570510+gflores34@users.noreply.github.com>
2023-11-14 18:44:53 -0600

    added ISBN to query

--- scripts/get_all_genre_books.php
+++ scripts/get_all_genre_books.php
@@ -18,7 +18,7 @@ if (mysqli_connect_errno())
 
 $genre = $_GET['genre'];
 
-$query="SELECT Books.Title, Books.SellingPrice, Authors.Name, Images.ImagePath FROM Books INNER JOIN Images ON Books.ISBN = Images.ISBN INNER JOIN Authors ON Books.AuthorId
+$query="SELECT Books.Title, Books.SellingPrice, Authors.Name, Images.ImagePath, Books.ISBN FROM Books INNER JOIN Images ON Books.ISBN = Images.ISBN INNER JOIN Authors ON Books.AuthorId
 = Authors.AuthorId WHERE Books.BookSubject = \"$genre\"";
 
 $result = $db_conn->query($query);
