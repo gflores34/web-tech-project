5d8c811a94951bb3f9576184a537eba1e61984ef
Austin Moore <austin19moore@gmail.com>
2023-11-30 22:21:48 -0600

    Fixed featured books

--- scripts/index.js
+++ scripts/index.js
@@ -63,54 +63,48 @@ function set_author(title, book_id){
     xhttp.send();
 }
 
-function set_featured_books(book1, book2, book3, book4){
+function set_featured_books(books){
 
-    getBookList(function(books) {
-        if (books.find((book) => book.Title === book1) !== undefined) {
-            set_author(book1, "featured1-author");
-            set_image(book1, "featured1-image");
-            set_price(book1, "featured1-price");
-            let title_link = document.getElementById("featured1-title");
-            title_link.innerHTML = book1;
-            set_book_isbn(book1, title_link);
-        } else {
-            document.getElementById("featured-1").style.visibility = "hidden";
+    // now ignoring passed books, just displaying books with id 1,2,3,4
+
+    var node = document.getElementById("featured-books");
+        for (let i = 0; i < books.length; i += 4) {
+            console.log(books[i]);
+            var newBook = document.createElement("div");
+            newBook.classList.add("book");
+
+            var image = document.createElement("img");
+            image.classList.add("cover-image");
+            console.log(books[i].ImagePath);
+            image.src = books[i].ImagePath;
+            newBook.appendChild(image);
+
+            var br = document.createElement("br");
+            newBook.appendChild(br);
+
+            var p1 = document.createElement("a");
+            p1.classList.add("book-title");
+            p1.href = "./book.html?" + books[i].ISBN;
+            var titleText = document.createTextNode(books[i].Title);
+            p1.appendChild(titleText);
+            newBook.appendChild(p1);
+
+            var p2 = document.createElement("p");
+            p2.classList.add("book-author");
+            var authorText = document.createTextNode(books[i].Name);
+            p2.appendChild(authorText);
+            newBook.appendChild(p2);
+
+            var p3 = document.createElement("p");
+            p3.classList.add("price");
+            var priceText = document.createTextNode("$" + books[i].SellingPrice);
+            p3.appendChild( priceText);
+            newBook.appendChild(p3);
+
+            node.appendChild(newBook);
+
         }
 
-        if (books.find((book) => book.Title === book2) !== undefined) {
-            set_author(book2, "featured2-author");
-            set_image(book2, "featured2-image");
-            set_price(book2, "featured2-price");
-            let title_link2 = document.getElementById("featured2-title");
-            title_link2.innerHTML = book2;
-            set_book_isbn(book2, title_link2);
-        } else {
-            document.getElementById("featured-2").style.visibility = "hidden";
-        }
-
-        if (books.find((book) => book.Title === book3) !== undefined) {
-            set_author(book3, "featured3-author");
-            set_image(book3, "featured3-image");
-            set_price(book3, "featured3-price");
-            let title_link3 = document.getElementById("featured3-title");
-            title_link3.innerHTML = book3;
-            set_book_isbn(book3, title_link3);
-        } else {
-            document.getElementById("featured-3").style.visibility = "hidden";
-        }
-
-        if (books.find((book) => book.Title === book4) !== undefined) {
-            set_author(book4, "featured4-author");
-            set_image(book4, "featured4-image");
-            set_price(book4, "featured4-price");
-            let title_link4 = document.getElementById("featured4-title");
-            title_link4.innerHTML = book4;
-            set_book_isbn(book4, title_link4);
-        } else {
-            document.getElementById("featured-4").style.visibility = "hidden";
-        }
-
-    });
 }
 
 function set_book_isbn(title, element){
@@ -134,6 +128,7 @@ function get_all_books(){
         var dataParse = JSON.parse(data);
 	
         generate_books(dataParse);
+        set_featured_books(dataParse);
 
         //document.getElementById(bookId).innerHTML = this.responseText;
     }
@@ -210,4 +205,3 @@ function genreHandler(){
 
 //genreHandler();
 get_all_books();
-set_featured_books("Harry Potter and the Sorcerer's Stone", "Dune", "The Road", "A Song of Ice and Fire");
